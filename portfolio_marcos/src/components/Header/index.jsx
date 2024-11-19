8d801d1057b76d93cd289c837603d69afece4bd0
Lucas Souza Duarte <lucas.souzaduarte.73@gmail.com>
2024-11-19 16:19:51 -0300

    feat: add pages and components folder to project

---
+++ portfolio_marcos/src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <div className="container">
+      <nav>
+        <button>Início</button>
+        <button>Meus Projetos</button>
+      </nav>
+    </div>
+  );
+};
